3a28bdb9dbc9e371ca9c3120444734d531932287
arrrrrmin <info@dotarmin.info>
2024-02-11 09:16:41 +0100

    💎: More precise guest genre mappings

--- src/lanz_mining/database/mappings.py
+++ src/lanz_mining/database/mappings.py
@@ -1,8 +1,8 @@
 # Party membership mapping aims to resolve the fuzzy role "politician".
+# Other maps aim to find a more general role like "agrar" or "industry"
+from datetime import date
 
-import datetime
-
-today = datetime.date.today().strftime("%Y-%m-%d")
+today = date.today().strftime("%Y-%m-%d")
 party_membership_map = {
     "Alexander Graf Lambsdorff": "FDP",
     "Aminata Touré": "B90G",
@@ -78,31 +78,110 @@ party_membership_map = {
     "Winfried Kretschmann": "B90G",
     "Wolfgang Kubicki": "FDP",
 }
+politics_keywords = [
+    "politiker",
+    "generalsekretär",
+    "landrätin",
+    "landrat",
+    "minister",
+    "kanzler",
+    "linke",
+    "spd",
+    "grüne",
+    "csu",
+    "freie wähler",
+    "fdp",
+    "cdu",
+    "bsw",
+    "afd",
+    "bundespräsident",
+    "politik",
+    "diplomat",
+]
+role_genre_map = {
+    "Aktivismus": lambda role: any(
+        _ in role.lower() for _ in ["aktivist", "bürgerrechtler", "whistleblow"]
+    ),
+    "Journalismus": lambda role: any(
+        _ in role.lower() for _ in ["reporter", "journalist", "korrespondent"]
+    ),
+    "Recht": lambda role: any(
+        _ in role.lower() for _ in ["jurist", "richter", "rechtsanwalt", "rechtsanwältin"]
+    ),
+    "Politik": lambda role: any(_ in role.lower() for _ in politics_keywords),
+    "Bildung": lambda role: any(
+        _ in role.lower() for _ in ["lehrer", "pädagog", "schüler", "student", "schulleiter"]
+    ),
+    "Geisteswissenschaft": lambda role: any(
+        _ in role.lower()
+        for _ in [
+            "philosoph",
+            "theolog",
+            "politolog",
+            "politikwissenschaft",
+            "islamwissenschaft",
+            "migrationswissenschaft",
+            "mirgationsforscher",
+            "extremismusforscher",
+        ]
+    ),
+    "Ethik": lambda role: any(_ in role.lower() for _ in ["ethik"]),
+    "Militär": lambda role: any(
+        _ in role.lower() for _ in ["militär", "verteidigung", "bundeswehr"]
+    ),
+    "Geschichte": lambda role: any(
+        _ in role.lower() for _ in ["historiker", "zeitzeuge", "zeitzeugin", "shoah"]
+    ),
+    "Soziales": lambda role: any(
+        _ in role.lower()
+        for _ in ["soziolog", "sozialwissenschaft", "sozialpsycholog", "sozialarbeiter"]
+    ),
+    "Naturwissenschaft": lambda role: any(
+        _ in role.lower() for _ in ["wissenschaft", "forscher", "physiker", "biolog"]
+    ),
+    "Literatur": lambda role: any(
+        _ in role.lower() for _ in ["autor", "schriftsteller", "publizist"]
+    ),
+    "Agrar": lambda role: any(_ in role.lower() for _ in ["agrar", "landwirt", "förster"]),
+    "Ökonomie": lambda role: any(
+        _ in role.lower()
+        for _ in [
+            "wirtschaft",
+            "ökonom",
+            "vorstand",
+            "vw-chef",
+            "manager",
+            "industrie",
+            "unternehmer",
+            "unternehmensberater",
+        ]
+    ),
+    "Gesundheit": lambda role: any(
+        _ in role.lower()
+        for _ in [
+            "arzt",
+            "ärztin",
+            "pharmazeut",
+            "psychiater",
+            "mediziner",
+            "virolog",
+            "psycholog",
+            "gynäkolog",
+            "pharmakolog",
+            "gesundheit",
+        ]
+    ),
+    "Inneres": lambda role: any(
+        _ in role.lower() for _ in ["sicherheitsexpert", "polizei", "polizist", "kriminal"]
+    ),
+}
 
 
-def create_complicated_party_memberships():
+def get_complicated_party_memberships():
+    """Special cases of politicians switching their party membership at some point in time."""
     return {
         "Boris Palmer": [("1972-1-1", "2023-5-1", "B90G"), ("2023-1-5", today, "No party")],
         "Claus Ruhe Madsen": [("1972-1-1", "2023-5-1", "No party"), ("2023-5-1", today, "CDU")],
         "Jörg Meuthen": [("2015-7-1", "2022-1-28", "AFD"), ("2022-1-28", today, "No party")],
         "Sahra Wagenknecht": [("1969-7-1", "2024-1-8", "LINKE"), ("2024-1-8", today, "BSW")],
     }
-
-
-def find_party_membership(name: str, date: str) -> str or None:
-    membership = None
-    if name in party_membership_map.keys():
-        membership = party_membership_map[name]
-    elif name in party_membership_map.keys():
-        compilcated_membership_map = create_complicated_party_memberships()
-        if name in compilcated_membership_map.keys():
-            membership_ranges = compilcated_membership_map[name]
-            for start, end, party in membership_ranges:
-                start = datetime.datetime.strptime(start, "%Y-%m-%d")
-                end = datetime.datetime.strptime(end, "%Y-%m-%d")
-                date = datetime.datetime.strptime(date, "%Y-%m-%d")
-                if start <= date < end:
-                    membership = party
-                    break
-
-    return membership
