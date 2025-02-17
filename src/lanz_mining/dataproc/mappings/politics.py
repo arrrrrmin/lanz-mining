a704eebb2559ca325e1a3d977666f630290ba118
arrrrrmin <info@dotarmin.info>
2025-02-17 13:01:12 +0100

    ✨(parsers): Parser, register and mappings for 'hartaberfair'

--- src/lanz_mining/dataproc/mappings/politics.py
+++ src/lanz_mining/dataproc/mappings/politics.py
@@ -161,6 +161,14 @@ PARTY_MEMBERSHIP_MAP = {
     "Matthias Miersch": Party.SPD,
     "Malu Dreyer": Party.SPD,
     "Renate Künast": Party.B90G,
+    "Cansel Kiziltepe": Party.SPD,
+    "Armin Schuster": Party.CDU,
+    "Dorothee Bär": Party.CDU,
+    "Christian Leye": Party.BSW,
+    "Lamya Kaddor": Party.B90G,
+    "Julia Klöckner": Party.CDU,
+    "Katarina Barley": Party.SPD,
+    "Ines Schwerdtner": Party.LINKE,
 }
 
 
